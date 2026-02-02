3517f052652b5ddc39f78f0f82b164abfaf139d3
Gabriel-1234 <59393686+Gabriel-1234@users.noreply.github.com>
2026-02-02 12:51:14 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,12 +7,12 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5002',
+        target: 'https://cash-app-api.up.railway.app/api',
         changeOrigin: true
       }
     }
   },
   define: {
-    'process.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL || 'http://a8cosos0ogw80cw0skgk0kc8.127.0.0.1.sslip.io:5002/api')
+    'process.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL || 'https://cash-app-api.up.railway.app/api')
   }
 });
